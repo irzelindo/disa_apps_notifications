0f4e2c0a48fd9070151a2c7eb906fbd6011a6223
irzelindo salvador <irzelindo.salvador@gmail.com>
2022-10-28 12:06:00 +0200

    Add email functionality

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ import pandas as pd
 from controllers.process import *
 from controllers.email import *
 from configs.processes import processes
+from configs.paths import *
+from configs.addresses import *
+
+
+pd.set_option('display.max_columns', None)
 
 df = pd.DataFrame()
+
+SUBJECT = f'{LAB_NAME} Processes Monitor'
+TEXT = f'Hello,\n\nThis is an automated email to inform you about the running process:\n\n Regards,\n\n{LAB_NAME}'
+EMAILS = EMAILS_ADDR
+CC_EMAILS = EMAILS_CC_ADDR
 
 for process in processes:
     if check(process['name']):
@@ -12,6 +22,19 @@ for process in processes:
     else:
         # print(f"Process {process['name']} from {process['path']} is not running")
         df = add_process(process['name'], process['path'], df)
-        # start(process['name'])
+        start(process['name'], process['path'], df)
 
-print(df.head())
+
+send_email(
+    OUTLOOK_HOST,
+    PORT,
+    OUTLOOK_EMAIL_USERNAME,
+    OUTLOOK_EMAIL_PASSWORD,
+    SUBJECT,
+    EMAILS,
+    CC_EMAILS,
+    TEXT,
+    None,
+    None,
+    df
+)
